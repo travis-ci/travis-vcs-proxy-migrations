8f5b959f3fde9adc660a073dee12786060dc041d
Stanislav Kolotinskiy <stanislav@assembla.com>
2021-07-29 17:02:16 +0300

    Fix index on commits table

--- db/migrate/20210615082201_create_commits.rb
+++ db/migrate/20210615082201_create_commits.rb
@@ -13,6 +13,6 @@ class CreateCommits < ActiveRecord::Migration[6.1]
     add_foreign_key :commits, :refs
     add_index :commits, :user_id
     add_index :commits, :repository_id
-    add_index :commits, :ref_id
+    add_index :commits, [:ref_id, :sha]
   end
 end
